dd8dc9e14c25347882d7e5a0e5f83b14099127d6
Asif Khan <beingasifk07@gmail.com>
2024-02-16 19:33:11 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ooh">
+      <h1>
+        <img
+          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Objects/Money%20Bag.webp"
+          alt="Money With Bag"
+          width="55"
+          height="55"
+        />
+        &nbsp;Expense Tracker&nbsp;
+        <img
+          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Objects/Money%20Bag.webp"
+          alt="Money With Bag"
+          width="55"
+          height="55"
+        />
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
